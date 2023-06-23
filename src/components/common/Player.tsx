0f3c198453f582edc48e009d7ad5dcfbff9fdbf4
acrichards3 <alexchristopherrichards@gmail.com>
2023-06-23 11:39:19 -0400

    animated ghosts multidirectional

--- src/components/common/Player.tsx
+++ src/components/common/Player.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './Player.module.scss';
 
 interface PlayerProps {
-  isPlaying: boolean;
+  x: number;
+  y: number;
+  status: 'loading' | 'moving' | 'dead';
 }
 
 export default function Player(props: PlayerProps) {
+  const mouthClasses = classNames(styles.mouth, {
+    [styles.loading as string]: props.status === 'loading',
+    [styles.moving as string]: props.status === 'moving',
+    [styles.dead as string]: props.status === 'dead',
+  });
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    console.log(e.key);
+  };
+
+  console.log('PLAYER POSITION:', `X: ${props.x}, Y: ${props.y}`);
+
   return (
-    props.isPlaying && (
-      <div className={styles.player}>
-        <div className={styles.mouth}></div>
-      </div>
-    )
+    <div className={styles.player} onKeyDown={handleKeyDown}>
+      <div className={mouthClasses}></div>
+    </div>
   );
 }
