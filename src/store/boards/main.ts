405186c217f4651c13ae0461d0b585571fcad075
acrichards3 <alexchristopherrichards@gmail.com>
2023-06-23 12:40:16 -0400

    build fix

--- src/store/boards/main.ts
+++ src/store/boards/main.ts
@@ -6,6 +6,19 @@ interface Board {
   setBoard: (board: BoardConfig[][]) => void;
 }
 
+// Z = Red Ghost spawn
+// X = Pink Ghost spawn
+// C = Blue Ghost spawn
+// V = Orange Ghost spawn
+// E = Ghost exit
+// S = Pacman spawn
+// P = Portal
+// B = Border
+// . = Empty
+// W = Wall
+// o = Pellet
+// O = Power Pellet
+
 export const useMain = create<Board>((set) => ({
   board: [
     ['B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', '.', 'B', 'P', 'B', '.', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B',], // prettier-ignore
@@ -25,8 +38,8 @@ export const useMain = create<Board>((set) => ({
     ['.', '.', '.', 'B', 'o', 'o', 'o', 'o', 'o', 'W', 'W', 'o', 'W', 'o', 'W', 'W', 'W', 'W', 'W', 'o', 'W', 'o', 'W', 'W', 'o', 'o', 'o', 'o', 'o', 'B', '.', '.', '.',], // prettier-ignore
     ['B', 'B', 'B', 'B', 'o', 'W', 'W', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'W', 'o', 'W', 'W', 'o', 'W', 'W', 'W', 'o', 'B', 'B', 'B', 'B',], // prettier-ignore
     ['P', '.', '.', '.', 'o', 'W', '.', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'B', 'B', 'E', 'B', 'B', 'o', 'W', 'o', 'W', 'W', 'o', 'W', '.', 'W', 'o', '.', '.', '.', 'P',], // prettier-ignore
-    ['B', 'B', 'B', 'B', 'o', 'W', 'W', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'B', 'G', '.', 'G', 'B', 'o', 'W', 'o', 'W', 'W', 'o', 'W', 'W', 'W', 'o', 'B', 'B', 'B', 'B',], // prettier-ignore
-    ['.', '.', '.', 'B', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'O', 'W', 'o', 'B', 'G', '.', 'G', 'B', 'o', 'W', 'O', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'B', '.', '.', '.',], // prettier-ignore
+    ['B', 'B', 'B', 'B', 'o', 'W', 'W', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'B', 'Z', '.', 'X', 'B', 'o', 'W', 'o', 'W', 'W', 'o', 'W', 'W', 'W', 'o', 'B', 'B', 'B', 'B',], // prettier-ignore
+    ['.', '.', '.', 'B', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'O', 'W', 'o', 'B', 'C', '.', 'V', 'B', 'o', 'W', 'O', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'B', '.', '.', '.',], // prettier-ignore
     ['B', 'B', 'B', 'B', 'o', 'W', 'W', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'B', 'B', 'B', 'B', 'B', 'o', 'W', 'o', 'W', 'W', 'o', 'W', 'W', 'W', 'o', 'B', 'B', 'B', 'B',], // prettier-ignore
     ['B', 'o', 'o', 'o', 'o', 'W', '.', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'o', 'o', '.', 'o', 'o', 'o', 'W', 'o', 'W', 'W', 'o', 'W', '.', 'W', 'o', 'o', 'o', 'o', 'B',], // prettier-ignore
     ['B', 'o', 'W', 'W', 'W', 'W', '.', 'W', 'o', 'W', 'W', 'o', 'W', 'o', 'W', 'W', 'W', 'W', 'W', 'o', 'W', 'o', 'W', 'W', 'o', 'W', '.', 'W', 'W', 'W', 'W', 'o', 'B',], // prettier-ignore
